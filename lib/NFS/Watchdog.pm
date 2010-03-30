d3e036e7bbed8f4cbd6fff59e83494935bb28f5d
Otvio Fernandes <otaviof@gmail.com>
2010-03-30 18:15:07 -0300

    adding tests for new main class, and removing some anoying default tests (for a while)

--- lib/NFS/Watchdog.pm
+++ lib/NFS/Watchdog.pm
@@ -1,107 +1,36 @@
 package NFS::Watchdog;
 
+#
+#  Author: Otavio Fernandes <otaviof@gmail.com>
+# Created: 03/30/2010 16:06:14
+#
+
 use warnings;
 use strict;
 
-=head1 NAME
-
-NFS::Watchdog - The great new NFS::Watchdog!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
 our $VERSION = '0.01';
 
+use Moose;
+use AnyEvent;
 
-=head1 SYNOPSIS
+has 'nfs_dir' => (
+    is       => 'rw',
+    isa      => 'Str',
+    required => 1,
+    trigger  => sub {
+        my ( $self, $dir ) = @_;
+        my $opendir_ready = AnyEvent->condvar;
+        my $opendir       = AnyEvent->io(
+            fh   => $dir,
+            poll => "r",
+            cb   => sub { $opendir_ready->send; }
+        );
+        sleep 2 and undef $opendir_ready;
+        return if ( !$opendir );
+        return 1;
+    },
+);
 
-Quick summary of what the module does.
+1;
 
-Perhaps a little code snippet.
-
-    use NFS::Watchdog;
-
-    my $foo = NFS::Watchdog->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Otavio Fernandes, C<< <otaviof at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-nfs-watchdog at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=NFS-Watchdog>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc NFS::Watchdog
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=NFS-Watchdog>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/NFS-Watchdog>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/NFS-Watchdog>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/NFS-Watchdog/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2010 Otavio Fernandes, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of NFS::Watchdog
+__END__
--- t/001-nfs_watchdog_new.t
+++ t/001-nfs_watchdog_new.t
@@ -1,18 +1,32 @@
 #
-#  Author: Otavio Fernandes <otavio.fernandes@locaweb.com.br>
+#  Author: Otavio Fernandes <otaviof@gmail.com>
 # Created: 03/29/2010 15:45:08
 #
 
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 4;
 
 BEGIN {
     use_ok('NFS::Watchdog');
     use NFS::Watchdog;
 }
 
-my $watchdog = new_ok('NFS::Watchdog');
+my $w1;
+eval { $w1 = new NFS::Watchdog(); };
+
+ok( !$w1, "Should Fail, there isn't no directory to start." );
+
+my $w2;
+eval {
+    $w2
+        = new NFS::Watchdog(
+        '/tmp/' . int( rand(100) ) . '/' . int( rand(100) ) );
+};
+
+ok( !$w2, "Should Fail, directory doesn't exists." );
+
+new_ok( 'NFS::Watchdog', [ { nfs_dir => '/tmp' } ] );
 
 __END__
