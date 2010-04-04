a98df0adbee8f9af9ca58e4ca01881187cd6a8d0
Otávio Fernandes <otaviof@gmail.com>
2010-04-04 19:03:40 -0300

    parsing NFS mount points from mounts file, incluind all tets need for that.

--- lib/NFS/Watchdog/Utils.pm
+++ lib/NFS/Watchdog/Utils.pm
@@ -10,6 +10,44 @@ use warnings;
 
 use Moose;
 
+has 'mounts' => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => '/proc/mounts',
+    trigger => sub {
+        my ( $self, $path ) = @_;
+        die "File does not exists: $path"
+            if ( $path and !-f $path );
+    },
+);
+
+sub parse_nfs_from_mounts {
+    my ($self) = @_;
+    my $rtrn;
+    open my $fh, '<', $self->mounts
+        or return;
+    while (<$fh>) {
+        chomp;
+        next
+            if (
+            !/^ (\S+:\S+)\s+
+                (\/\S+)\s+nfs\s+
+                ((rw|ro),.*?,addr=(\S+))\s+\d\s+\d$
+                /xs
+            );
+        push @{$rtrn},
+            {
+            origin      => $1,
+            destination => $2,
+            type        => $4,
+            server      => $5,
+            options     => $3
+            };
+    }
+    close($fh);
+    return $rtrn;
+}
+
 1;
 
 __END__
